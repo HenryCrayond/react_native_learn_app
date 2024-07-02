4044f4964f876e9773f872881a211901de72a7d7
Henry <henry@crayond.co>
2024-07-02 16:33:37 +0530

    completed

--- src/listingItems/index.tsx
+++ src/listingItems/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FlatList, Image, StatusBar, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, Pressable, StatusBar, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 import styled from 'styled-components/native';
 import LeftArrow from '../../assets/image/left-arrow.png';
 import PinLocation from '../../assets/image/location-pin.png';
@@ -15,6 +15,7 @@ import Increase from '../../assets/image/plus-circle.png';
 import Decreacse from '../../assets/image/minus-circle.png';
 import arrow1 from '../../assets/image/arrow-right-1.png';
 import arrow2 from '../../assets/image/arrow-right-2.png';
+import Decreacse0 from '../../assets/image/minus-circle-0.png';
 
 
 
@@ -98,22 +99,22 @@ const MenuCardtView = styled.View`
 
 const DATA = [
     {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        id: '1',
         title: 'Combo',
         icon: "🍗"
     },
     {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        id: '2',
         title: 'Special',
         icon: "👍"
     },
     {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        id: '3',
         title: 'Drinks',
         icon: "🍵"
     },
     {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        id: '4',
         title: 'Sanveg',
         icon: "🥪"
     },
@@ -144,7 +145,7 @@ const MenuData = [
         items: ['2 cheken wings', '2 raice'],
         price: '$12.00',
         icon: menu1,
-        count: 0
+        count: 10
     },
     {
         id: '2',
@@ -152,7 +153,7 @@ const MenuData = [
         items: ['1 cheken large', '2 raice'],
         price: '$18.00',
         icon: menu2,
-        count: 0
+        count: 6
     },
     {
         id: '3',
@@ -168,7 +169,55 @@ const MenuData = [
         items: ['2 cheken wings', '2 raice'],
         price: '$ 12.00',
         icon: menu4,
+        count: 9
+    },
+    {
+        id: '5',
+        dishName: 'Super Family Package',
+        items: ['2 cheken wings', '2 raice'],
+        price: '$12.00',
+        icon: menu1,
+        count: 2
+    },
+    {
+        id: '6',
+        dishName: 'Chicken Super Combo',
+        items: ['1 cheken large', '2 raice'],
+        price: '$18.00',
+        icon: menu2,
         count: 0
+    },
+    {
+        id: '7',
+        dishName: 'Checken Streak Large',
+        items: ['2 cheken wings', '2 raice'],
+        price: '$ 12.00',
+        icon: menu3,
+        count: 5
+    },
+    {
+        id: '8',
+        dishName: 'Special Full Checken',
+        items: ['2 cheken wings', '2 raice'],
+        price: '$ 12.00',
+        icon: menu4,
+        count: 0
+    },
+    {
+        id: '9',
+        dishName: 'Super Family Package',
+        items: ['2 cheken wings', '2 raice'],
+        price: '$12.00',
+        icon: menu1,
+        count: 10
+    },
+    {
+        id: '10',
+        dishName: 'Chicken Super Combo',
+        items: ['1 cheken large', '2 raice'],
+        price: '$18.00',
+        icon: menu2,
+        count: 8
     },
 ];
 
@@ -183,22 +232,24 @@ const BottomButton = () => {
                     </ChipView>
                 </RowDireection>
                 <RowDireection style={{ gap: 0 }}>
-                    <Image source={arrow2} style={{ height: 20, width: 10, borderRadius: 8 }} />
-                    <Image source={arrow1} style={{ height: 20, width: 10, borderRadius: 8 }} />
+                    <Image source={arrow2} style={{ height: 20, width: 12 }} />
+                    <Image source={arrow1} style={{ height: 20, width: 12 }} />
                 </RowDireection>
             </TouchableOpacity>
         </View>
     );
 };
 
-const Chip = ({ title, icon, color }: any) => (
-    <ChipView style={{ marginLeft: 10, backgroundColor: color }}>
-        <SubTitle style={{ color: color ? '#fff' : "", fontWeight: 500 }}>{title}</SubTitle>
-        <SubTitle style={{ marginLeft: 8 }}>{icon}</SubTitle>
-    </ChipView>
+const Chip = ({ title, icon, color, handleSelectMenu }: any) => (
+    <Pressable onPress={handleSelectMenu}>
+        <ChipView style={{ marginLeft: 10, backgroundColor: color }}>
+            <SubTitle style={{ color: color === '#ffff' ? '#4E585E' : "#ffff", fontWeight: 500 }}>{title}</SubTitle>
+            <SubTitle style={{ marginLeft: 8 }}>{icon}</SubTitle>
+        </ChipView>
+    </Pressable>
 );
 
-const ListMenuItem = ({ dishName, icon, items, count, price }: any) => (
+const ListMenuItem = ({ dishName, icon, items, count, price, handleIncrease, handleDecrease }: any) => (
     <MenuCardtView style={{ marginBottom: 20 }}>
         <RowDireection style={{ justifyContent: "space-between", width: "100%" }}>
             <Image source={icon} style={{ height: 70, width: 70, borderRadius: 8 }} />
@@ -214,9 +265,13 @@ const ListMenuItem = ({ dishName, icon, items, count, price }: any) => (
             </View>
 
             <RowDireection style={{ gap: 10 }}>
-                <Image source={Decreacse} style={{ height: 22, width: 22, borderRadius: 8 }} />
-                <StyledText>{count}</StyledText>
-                <Image source={Increase} style={{ height: 20, width: 20, borderRadius: 8 }} />
+                <Pressable onPress={handleDecrease}>
+                    <Image source={count == 0 ? Decreacse0 : Decreacse} style={{ height: 22, width: 22, borderRadius: 8 }} />
+                </Pressable>
+                <PageHeadingText>{count}</PageHeadingText>
+                <Pressable onPress={handleIncrease}>
+                    <Image source={Increase} style={{ height: 20, width: 20, borderRadius: 8 }} />
+                </Pressable>
             </RowDireection>
         </RowDireection>
     </MenuCardtView>
@@ -224,6 +279,42 @@ const ListMenuItem = ({ dishName, icon, items, count, price }: any) => (
 
 const ListingItems = () => {
     const [selectItem, setSelectitem] = useState(['Combo'])
+    const [listMenu, setListMenu] = useState<any>(MenuData)
+
+    const handleSelectAll = () => {
+        if (selectItem?.length > 1) {
+            setSelectitem([''])
+        } else {
+            setSelectitem(DATA?.map((val) => val.title))
+        }
+    }
+
+    const handleSelectMenu = (id: string) => {
+        const findTitle = DATA.filter((val) => val.id !== id)
+        if (findTitle) {
+            setSelectitem(findTitle.map((val) => val.title))
+        } else {
+            setSelectitem((prev) => ([...prev, ...findTitle]))
+        }
+    }
+
+    const handleIncrease = (id: string) => {
+        const addCount = listMenu.map((val: any) => {
+            val.id == id ? val.count = val.count + 1 : val.count
+            return val;
+        })
+        setListMenu(addCount)
+
+    }
+
+    const handleDecrease = (id: string) => {
+        const lessCount = listMenu.map((val: any) => {
+            val.id == id ? val.count = val.count - 1 : val.count
+            return val;
+        })
+        setListMenu(lessCount)
+    }
+
 
     return (
         <Container>
@@ -246,7 +337,7 @@ const ListingItems = () => {
                 <View style={{ ...styles.hairline, marginTop: 14 }}></View>
                 <CenterView style={{ flexDirection: "row", marginTop: 18 }}>
                     {
-                        Ratings?.map((items, i,{length}) => (
+                        Ratings?.map((items, i, { length }) => (
                             <>
                                 <View key={i}>
                                     <RowDireection>
@@ -255,20 +346,22 @@ const ListingItems = () => {
                                     </RowDireection>
                                     <SubTitle style={{ marginTop: 10 }}>{items?.label}</SubTitle>
                                 </View>
-                               {length !== i+1 &&  <View style={{...styles.vartiline}}></View>}
+                                {length !== i + 1 && <View style={{ ...styles.vartiline }}></View>}
                             </>
                         ))
                     }
                 </CenterView>
                 <View style={{ ...styles.hairline, marginTop: 14 }}></View>
                 <RowDireection style={{ gap: 6, marginTop: 20 }}>
-                    <ChipView style={{ backgroundColor: "" }}>
-                        <SubTitle style={{ color: "" }}>All</SubTitle>
-                    </ChipView>
+                    <Pressable onPress={handleSelectAll}>
+                        <ChipView style={{ backgroundColor: "" }}>
+                            <SubTitle style={{ color: "#4E585E" }}>All</SubTitle>
+                        </ChipView>
+                    </Pressable>
                     <FlatList
                         data={DATA}
                         horizontal
-                        renderItem={({ item }) => <Chip title={item.title} icon={item.icon} color={selectItem?.includes(item?.title) ? "#CE4B36" : ""} />}
+                        renderItem={({ item }) => <Chip title={item.title} icon={item.icon} color={selectItem?.includes(item?.title) ? "#CE4B36" : "#ffff"} handleSelectMenu={() => handleSelectMenu(item.id)} />}
                         keyExtractor={item => item.id}
                     />
                 </RowDireection>
@@ -281,8 +374,8 @@ const ListingItems = () => {
                 </RowDireection>
                 <View>
                     <FlatList
-                        data={MenuData}
-                        renderItem={({ item }) => <ListMenuItem dishName={item.dishName} icon={item.icon} items={item.items} count={item.count} price={item.price} />}
+                        data={listMenu}
+                        renderItem={({ item }) => <ListMenuItem dishName={item.dishName} icon={item.icon} items={item.items} count={item.count} price={item.price} handleIncrease={() => handleIncrease(item.id)} handleDecrease={() => handleDecrease(item.id)} />}
                         keyExtractor={item => item.id}
                     />
                 </View>
@@ -322,9 +415,9 @@ const styles = StyleSheet.create({
     },
     vartiline: {
         backgroundColor: '#D9DBDD',
-        width:1,
+        width: 1,
         height: '100%',
-        marginHorizontal:10
+        marginHorizontal: 10
     },
 });
 
